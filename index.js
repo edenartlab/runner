f34e4d6535104954b65c57e1d50ab60b62c8a812
Gene Kogan <kogan.gene@gmail.com>
2022-11-26 22:36:41 -0800

    post to get

--- index.js
+++ index.js
@@ -41,7 +41,9 @@ app.use(cors());
 app.use(express.json({limit: '50mb'}));
 app.use(express.urlencoded({limit: '50mb'}));
 
-app.post("/fetch", handleFetchRequest);
+app.get("/fetch", handleFetchRequest);
+
+//app.post("/fetch", handleFetchRequest);
 
 app.get("/", async (req, res) => {
   res.send("Gateway is running yay");
