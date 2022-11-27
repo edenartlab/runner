18e1a8c9224b59349ffce489b215dd3825d96a5b
Gene Kogan <kogan.gene@gmail.com>
2022-11-26 22:58:40 -0800

    with random prompt

--- index.js
+++ index.js
@@ -18,6 +18,84 @@ const PORT = 8000;
 
 let count = 0;
 
+
+
+const title_prompt = `A diverse set of highly detailed and imaginative titles of digital artworks I would like to make, one per line
+off-grid cyberpunk vanlife explorer surveying mars oasis
+photo of holy futuristic cyborg robot painter
+digital illustration of tokyo cityscape
+cyber neon AI art kid on vast barren desert
+portrait of a biomechanical head inside an orb
+lush tropical forest with strange birdlike creatures patrolling the canopies
+cyberpunk hero whiz kid in the neon city super high-tech future
+a film still of an adventurer kid with cyber gear riding an electric unicycle on a desert with rocks, cactus, mountains
+hyper mecha digital nomads clad in cyberpunk attire looking menacing
+off-grid survivalist scientists vanlife in a desert oasis
+epic winged Hippogriff flying over a medieval castle under a dark starred sky
+futuristic military cyborgs with high-tech gear in a desolate wasteland
+vibrant xenomorph with aurora inspired by fine greek sculpture
+cybernetic holographic ninja riding a motorcycle with a katana
+`;
+
+const modifier_prompt = `For each title, propose a set of detailed and relevant stylistic modifiers which create a unique aesthetic.
+Title: off-grid cyberpunk vanlife explorer surveying mars oasis
+Modifiers: storybook illustration, trending on pixiv, rendered 4k in Octane, raytracing
+Title: epic winged Hippogriff flying over a medieval castle under a dark starred sky
+Modifiers: magic realism, storybook fantasy, ink drawing, behance best of the week
+Title: portrait of a biomechanical head inside a futuristic space helmet
+Modifiers: Baroque, kinetic pointillism, watercolors, concept art, divine and awe-inspiring
+Title: lush tropical forest with strange birdlike creatures patrolling the canopies
+Modifiers: album cover, mix of afrofuturism and sōsaku hanga, detailed pencil sketch
+`;
+
+
+async function createPrompt() {
+  
+  const configuration = new openai.Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+  });
+  const openai_api = new openai.OpenAIApi(configuration);
+
+  let title = '';
+  let modifiers = '';
+
+  while (title.length == 0) {
+    let completion = await openai_api.createCompletion({
+      model: "text-davinci-002",
+      prompt: title_prompt,
+      temperature: 0.99,
+      max_tokens: 60,
+      top_p: 1,
+      frequency_penalty: 2.0,
+      presence_penalty: 1.0,
+      stop: ["\n"]
+    });
+    title = completion.data.choices[0].text;
+  }
+
+  while (modifiers.length == 0) {
+    const mod_prompt = `${modifier_prompt}\nTitle: ${title}\nModifiers: `
+    let completion = await openai_api.createCompletion({
+      model: "text-davinci-002",
+      prompt: mod_prompt,
+      temperature: 0.99,
+      max_tokens: 60,
+      top_p: 1,
+      frequency_penalty: 2.0,
+      presence_penalty: 1.0,
+      stop: ["\n"]
+    });    
+    modifiers = completion.data.choices[0].text;
+  }
+
+  const prompt = `${title}, ${modifiers}`
+
+  return prompt;
+}
+
+
+
+
 async function getAuthToken(data) {
   let response = await axios.post(GATEWAY_URL+'/sign_in', data)
   var authToken = response.data.authToken;
@@ -38,9 +116,11 @@ async function run_eden_job() {
     "apiSecret": API_SECRET
   };
 
+  let gen_prompt = await createPrompt();
+
   let config = {
     "mode": "generate", 
-    "text_input": "The quick brown fox jumps over the lazy dog.",
+    "text_input": gen_prompt,
     "sampler": "euler_ancestral",
     "scale": 10.0,
     "steps": 50, 
