a03b337916afd5dec99c53355d33fc34b3a1727c
Gene Kogan <kogan.gene@gmail.com>
2023-01-11 22:29:40 -0800

    new runner

---
+++ config_generators.js
@@ -0,0 +1,104 @@
+import openai from 'openai';
+import dotenv from 'dotenv';
+
+dotenv.config()
+
+const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
+
+
+function getRandomSample(arr, sampleSize) {
+  return arr
+    .sort(() => 0.5 - Math.random())
+    .slice(0, sampleSize);
+}
+
+const subjects = [
+  'Coders',
+  'Vanlife programming wizards',
+  'Coders on electric unicycles',
+  'Vanlife cyberpunk hackers',
+  'Vanlife cyberpunk coders',
+  'Intelligence as a service',
+  'Comptuer brains and artificial intelligences',
+  'Cyberpunk hackers',
+  'Solarpunk coders',
+  'Electric unicycle riding nomads',
+  'Solarpunk electric bike riding coder gang',
+  'The simulation',
+  'A simulation containing cyberpunk nomads',
+  'A simulation of a solarpunk building being constructed',
+  'Off-grid cyberpunk vanlife explorer surveying mars oasis',
+  'Holy futuristic cyborg',
+  'Cyber neon AI art kid',
+  'Portrait of a biomechanical head inside an orb',
+  'Lush tropical canopy with creatures',
+  'Cyberpunk hero whiz kid',
+  'Hyper mecha digital nomads clad in cyberpunk attire',
+  'Off-grid survivalist scientists vanlife',
+  'Epic winged Hippogriff flying over a medieval castle',
+  'Cybernetic holographic ninja riding a motorcycle with a kitana',
+];
+
+const contexts = [
+  'in the desert',
+  'in a solarpunk pavilion',
+  'in a solarpunk pavilion in the desert',
+  'in a lush zen buddhism garden',
+  'futuristic desert village',
+  'in a rustic Buddhist monastery',
+  'in the cyberpunk city',
+  'in a neon glow city',
+];
+
+const media = ['3D render', 'computer rendering', 'detailed drawing', 'detailed painting', 'digital painting', 'digital rendering', 'fine art painting', 'hologram', 'hyperrealistic painting', 'low poly render', 'macro photograph', 'painting', 'pastel', 'pencil sketch', 'photo', 'photorealistic painting', 'polaroid photo', 'pop art painting', 'poster', 'raytraced image', 'tilt shift photo', 'watercolor painting', 'wireframe diagram', 'acrylic painting', 'airbrush painting', 'album cover', 'ambient occlusion render', 'anime drawing', 'ultrafine detailed painting', 'chalk art', 'computer graphics', 'concept art', 'cyberpunk art', 'digital art', 'egyptiart', 'graffiti art', 'pixel art', 'poster art', 'vector art'];
+
+const flavors = ['vaporwave', 'cyberpunk', 'synthwave', '4k', 'aestheticism', 'arabesque', 'baroque', 'crystal cubism', 'futurism', 'hyperrealism', 'hypermodernism', 'retrofuturism', 'highly detailed', 'sharp focus', 'intricate', 'digital painting', 'illustration', 'octane render', 'smooth', '8 k', 'elegant', '8k', 'cinematic', '4k', 'fantasy', 'cinematic lighting', 'detailed', '4 k', 'photorealistic', 'unreal engine', 'masterpiece', 'realistic', 'hd', 'dramatic lighting', 'volumetric lighting', 'high detail', 'beautiful', 'hyperrealistic', 'hyper detailed', 'portrait', 'high quality', 'hyper realistic', 'ultra realistic', 'unreal engine 5', 'matte painting', 'ultra detailed', 'award winning', 'matte', 'intricate details', 'epic', 'hdr', 'extremely detailed', 'very detailed', 'oil painting', 'dynamic lighting', 'hyperdetailed', 'oil on canvas', 'high resolution', 'sharp', 'studio lighting', 'depth of field', 'hyper realism', 'full body', 'colorful', 'dark', 'cyberpunk', 'octane', 'centered', 'high contrast', 'bokeh', 'global illumination', 'dramatic', 'symmetrical', 'd & d', 'intricate detail', 'atmospheric', 'd&d', 'golden ratio', 'wide angle', 'anime', 'golden hour', 'detailed face', 'rule of thirds', 'stunning', 'vibrant colors', 'soft lighting', 'ray tracing', 'photography', 'epic composition', 'radiant light', '8k resolution', 'cinematic composition', 'horror', 'vibrant', 'black and white', 'close up', 'medium shot', 'ornate', 'photo realistic', 'photo', 'vivid colors', '8 k resolution', 'trending on art station', 'futuristic', 'high details', 'high definition', 'volumetric light', 'wide shot', 'movie still', 'establishing shot', 'surreal', 'gothic', '3 d render', 'unreal 5', 'photograph', 'sunset', 'painting', 'sci-fi', 'rim light', 'hearthstone', 'beautiful lighting', 'ambient lighting', 'sci - fi', 'moody', 'digital illustration', 'symmetrical face', 'epic lighting', 'fog', 'sakimichan', 'beautiful face', 'soft light', 'fine details', 'dark fantasy', 'gorgeous', 'deep focus', 'night', '3 d', 'eerie', 'daz', 'shallow depth of field', '3d', 'atmospheric lighting', 'symmetry', 'cute', 'studio quality', 'scifi', 'chiaroscuro', 'insanely detailed', 'white background', 'natural light', 'details', 'hard edges', 'character design', 'artistic', 'asymmetrical', 'rpg portrait', 'sunny day', 'ethereal', 'bright colors', 'raytracing', 'perfect face', 'god rays', 'psychedelic', '35mm', 'dramatic light', 'dynamic pose', '3 5 mm', 'steampunk', 'focus', 'extreme detail', 'lifelike', 'organic painting', 'backlit', '3d render', 'octane rendered', 'neon', 'foggy', 'uhd', '4k resolution', 'cinematic shot', 'breathtaking', 'vivid', 'fantasy', 'ultra wide angle', 'subsurface scattering', 'very coherent', 'stylized', 'ultra hd', 'aesthetic', 'lens flare', 'intricate linework', 'vintage', 'focused', 'insanely detailed and intricate', 'hq', 'ultrarealistic', 'dreamy', 'high fantasy', 'award winning photo', 'minimalist', '4 k resolution', 'award-winning', 'manga', 'realistic shaded perfect', 'professional photography', 'perfect composition', 'dslr', 'render', 'award winning photography', 'magical', 'abstract', 'detailed illustration', 'vaporwave', 'atmosphere', 'beautiful composition', 'bouguereau', 'glowing lights', 'cel shaded', 'painted', 'sense of awe', 'full of colour', 'detailed painting', 'synthwave', 'dark atmosphere', 'neo-gothic', 'realistic lighting', 'mystical', 'science fiction', 'cinematic atmosphere'];
+
+
+export async function generatePrompts(n) {
+  let prompts = [];
+  for (var i=0; i<n; i++) {
+    const subject = getRandomSample(subjects, 1);
+    const context = getRandomSample(contexts, 1);
+    const flavors2 = getRandomSample(flavors, 2);
+    const medium2 = getRandomSample(media, 2);
+    const prompt = `${subject} ${context}, ${medium2[0]}, ${flavors2[0]}, ${medium2[1]}, ${flavors2[1]}`;
+    prompts.push(prompt);
+  }
+  return prompts;
+}
+
+export async function createPrompt() {
+  
+  const prompts = await generatePrompts(6);
+  const combined_prompt = prompts.join('\n')+'\n';
+
+  // combine into one string with newlines
+  const initString = "Make a detailed imaginitive description of a detailed artwork in the following style. Here are some examples. Create another 1."
+  const promptString = initString + prompts.join('\n');
+
+  const configuration = new openai.Configuration({
+    apiKey: OPENAI_API_KEY,
+  });
+  const openai_api = new openai.OpenAIApi(configuration);
+
+  let title = '';
+
+  while (title.length == 0) {
+    let completion = await openai_api.createCompletion({
+      model: "davinci",
+      prompt: combined_prompt,
+      temperature: 0.99,
+      max_tokens: 60,
+      top_p: 1,
+      frequency_penalty: 2.0,
+      presence_penalty: 1.0,
+      stop: ["\n"]
+    });
+    title = completion.data.choices[0].text;
+  }
+
+  return title.trim();
+}
+  
+
--- index.js
+++ index.js
@@ -1,140 +1,19 @@
 import express from 'express';
+import dotenv from 'dotenv';
 import cors from 'cors';
-import axios from 'axios'
-import dotenv from 'dotenv'
-import openai from 'openai';
+import axios from 'axios';
+import {generatePrompts, createPrompt} from './config_generators.js';
 
 dotenv.config()
 
 const GATEWAY_URL = process.env.GATEWAY_URL;
-const MINIO_URL = process.env.MINIO_URL;
-const MINIO_BUCKET = process.env.MINIO_BUCKET;
 const API_KEY = process.env.API_KEY;
 const API_SECRET = process.env.API_SECRET;
-const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
 const PORT = process.env.PORT || 8000;
-
-// const model_name = "text-davinci-002";
-const model_name = "curie";
 
 let nMade = 0;
 let nRunning = 3;
 let nCompletions = 0;
-
-const title_prompt = `A diverse set of highly detailed and imaginative titles of digital artworks I would like to make, one per line.
-
-off-grid cyberpunk vanlife explorer surveying mars oasis
-photo of holy futuristic cyborg robot painter
-digital illustration of tokyo cityscape
-cyber neon AI art kid on vast barren desert
-portrait of a biomechanical head inside an orb
-lush tropical forest with strange birdlike creatures patrolling the canopies
-cyberpunk hero whiz kid in the neon city super high-tech future
-a film still of an adventurer kid with cyber gear riding an electric unicycle on a desert with rocks, cactus, mountains
-hyper mecha digital nomads clad in cyberpunk attire looking menacing
-off-grid survivalist scientists vanlife in a desert oasis
-epic winged Hippogriff flying over a medieval castle under a dark starred sky
-futuristic military cyborgs with high-tech gear in a desolate wasteland
-vibrant xenomorph with aurora inspired by fine greek sculpture
-cybernetic holographic ninja riding a motorcycle with a katana
-`;
-
-const modifier_prompt = `For each title, propose a set of detailed and relevant stylistic modifiers which create a unique aesthetic.
-
-Title: off-grid cyberpunk vanlife explorer surveying mars oasis
-Modifiers: storybook illustration, trending on pixiv, rendered 4k in Octane, raytracing
-Title: epic winged Hippogriff flying over a medieval castle under a dark starred sky
-Modifiers: magic realism, storybook fantasy, ink drawing, behance best of the week
-Title: portrait of a biomechanical head inside a futuristic space helmet
-Modifiers: Baroque, kinetic pointillism, watercolors, concept art, divine and awe-inspiring
-Title: lush tropical forest with strange birdlike creatures patrolling the canopies
-Modifiers: album cover, mix of afrofuturism and sōsaku hanga, detailed pencil sketch
-`;
-
-const question_prompt = `A diverse set of highly arresting and vexing philosophical questions, one per line.
-
-what is the meaning of life?
-why should there be any existence at all?
-is there a natural teleology to the universe?
-what is the true nature of consciousness?
-are we living in a simulation?
-`;
-
-
-async function createQuestion() {
-  const configuration = new openai.Configuration({
-    apiKey: OPENAI_API_KEY,
-  });
-  const openai_api = new openai.OpenAIApi(configuration);
-  let question = '';
-  while (question.length == 0) {
-    let completion = await openai_api.createCompletion({
-      model: model_name,
-      prompt: question_prompt,
-      temperature: 0.99,
-      max_tokens: 30,
-      top_p: 1,
-      frequency_penalty: 2.0,
-      presence_penalty: 1.0,
-      stop: ["\n"]
-    });
-    nCompletions += 1;
-    question = completion.data.choices[0].text;
-    console.log(`complete (${nCompletions}) question: ${question}`)
-    return question;
-  }
-}
-
-async function createPrompt() {
-  
-  const configuration = new openai.Configuration({
-    apiKey: OPENAI_API_KEY,
-  });
-  const openai_api = new openai.OpenAIApi(configuration);
-
-  let title = '';
-  let modifiers = '';
-
-  while (title.length == 0) {
-    let completion = await openai_api.createCompletion({
-      model: model_name,
-      prompt: title_prompt,
-      temperature: 0.99,
-      max_tokens: 60,
-      top_p: 1,
-      frequency_penalty: 2.0,
-      presence_penalty: 1.0,
-      stop: ["\n"]
-    });
-    nCompletions += 1;
-    title = completion.data.choices[0].text;
-    console.log(`complete (${nCompletions}) title: ${title}`)
-  }
-
-  while (modifiers.length == 0) {
-    const mod_prompt = `${modifier_prompt}Title: ${title}\nModifiers:`
-    let completion = await openai_api.createCompletion({
-      model: model_name,
-      prompt: mod_prompt,
-      temperature: 0.99,
-      max_tokens: 60,
-      top_p: 1,
-      frequency_penalty: 2.0,
-      presence_penalty: 1.0,
-      stop: ["\n"]
-    });    
-    nCompletions += 1;
-    modifiers = completion.data.choices[0].text;
-    console.log(`complete (${nCompletions}) modifiers: ${modifiers}`)
-  }
-
-  title = title.trim();
-  modifiers = modifiers.trim();
-
-  const prompt = `${title}, ${modifiers}`
-
-  return prompt;
-}
 
 async function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
@@ -153,10 +32,15 @@ async function startPrediction(data) {
 }
 
 
-async function run_eden_jobs(N) {
-  if (N == 0) {
-    return;
-  }
+const main = async () => {
+  const prompts = await generatePrompts(6);
+  const promptString = prompts.join('\n');
+  //const prompt = await createPrompt();
+
+  let prompt = await generatePrompts(1);
+  prompt = prompt[0];
+
+  console.log(prompt);
   
   let authData = {
     "apiKey": API_KEY, 
@@ -165,91 +49,31 @@ async function run_eden_jobs(N) {
   
   let authToken = await getAuthToken(authData);
 
-  let prompts = [];
-  for (var i = 0; i < N; i++) {
-    let genPrompt = await createPrompt();
-    console.log(`PROMPT: ${genPrompt}`)
-    if (genPrompt) {
-      prompts.push(genPrompt);
-    }
-  }
-
-  let predictions = [];
-
-  for (var i = 0; i < prompts.length; i++) {
-
-    let config = {
-      "mode": "generate", 
-      "text_input": prompts[i],
-      "sampler": "euler_ancestral",
-      "scale": 10.0,
-      "steps": 50, 
-      "width": 512,
-      "height": 512,
-      "seed": Math.floor(1e8 * Math.random())
-    }
-
-    const request = {
-      "token": authToken,
-      "application": "heartbeat", 
-      "generator_name": "stable-diffusion", 
-      "config": config,
-      "metadata": null
-    }
-
-    let response = await startPrediction(request);
-    let prediction_id = response.data;
-    predictions.push(prediction_id);
-    console.log(`job submitted, task id ${prediction_id}`);    
-  }
-
-  // also run oracle once
-  /*
-  let question = await createQuestion();
-
-  const oracle_config = {
-    "question": question,
-    "voice_embedding": "data/rivka_embedding.pkl",
-    "face": "data/oracle.jpg"
+  let config = {
+    "mode": "generate", 
+    "text_input": prompt,
+    "sampler": "euler_ancestral",
+    "scale": 10.0,
+    "steps": 50, 
+    "width": 512,
+    "height": 512,
+    "seed": Math.floor(1e8 * Math.random())
   }
 
   const request = {
     "token": authToken,
-    "application": "oracle", 
-    "generator_name": "oracle", 
-    "config": oracle_config,
+    "application": "heartbeat", 
+    "generator_name": "stable-diffusion", 
+    "config": config,
     "metadata": null
   }
-  let oracle_response = await startPrediction(request);
-  let oracle_prediction_id = oracle_response.data;
-  console.log(`oracle job submitted, task id ${oracle_prediction_id}`);
-  */
- 
-  // poll every few seconds for update to the job
 
-  while (predictions.length > 0) {
-    console.log("search")
-    console.log(predictions)
-    let response = await axios.post(GATEWAY_URL+'/fetch', {
-      "taskIds": predictions
-    });
-    for (var i = 0; i < response.data.length; i++) {
-      let {status, output} = response.data[i];
-      if (status == 'complete') {
-        let outputUrl = `${MINIO_URL}/${MINIO_BUCKET}/${output}`;
-        console.log(`finished! result at ${outputUrl}`);
-        nMade = nMade+1;
-        predictions.splice(i, 1);
-      }
-      else if (status == 'failed') {
-        console.log("failed");
-        predictions.splice(i, 1);
-      }
-    } 
-    await sleep(10000);
-  }
-
+  let response = await startPrediction(request);
+  let prediction_id = response.data;
+  console.log(`job submitted, task id ${prediction_id}`);
+  nCompletions++;
 }
+
 
 async function handleUpdate(req, res) {
   const {count, apiKey, apiSecret} = req.body;
@@ -267,7 +91,7 @@ app.use(cors());
 app.use(express.json({limit: '50mb'}));
 app.use(express.urlencoded({limit: '50mb'}));
 
-app.post("/update", handleUpdate);
+// app.post("/update", handleUpdate);
 
 app.get("/", async (req, res) => {
   res.send(`Runner has made ${nMade} creations so far. ${nRunning} threads are running. Completions ${nCompletions}.`);
@@ -276,9 +100,12 @@ app.get("/", async (req, res) => {
 app.listen(PORT, () => {
   console.log(`Runner is now listening on port ${PORT} !`);
   async function update() {
-    await run_eden_jobs(nRunning);
+    for (var i=0; i<nRunning; i++) {
+      await main();
+    }
     setTimeout(update, 300000);
     await sleep(5000);
   }
   update();
 });
+
