517ac50b4c30b2d2480a299083174bd077bd3171
Gene Kogan <kogan.gene@gmail.com>
2023-02-14 21:08:03 -0800

    update version

--- config_generators.js
+++ config_generators.js
@@ -52,10 +52,29 @@ const contexts = [
 
 const media = ['3D render', 'computer rendering', 'detailed drawing', 'detailed painting', 'digital painting', 'digital rendering', 'fine art painting', 'hologram', 'hyperrealistic painting', 'low poly render', 'macro photograph', 'painting', 'pastel', 'pencil sketch', 'photo', 'photorealistic painting', 'polaroid photo', 'pop art painting', 'poster', 'raytraced image', 'tilt shift photo', 'watercolor painting', 'wireframe diagram', 'acrylic painting', 'airbrush painting', 'album cover', 'ambient occlusion render', 'anime drawing', 'ultrafine detailed painting', 'chalk art', 'computer graphics', 'concept art', 'cyberpunk art', 'digital art', 'egyptiart', 'graffiti art', 'pixel art', 'poster art', 'vector art'];
 
-const flavors = ['vaporwave', 'cyberpunk', 'synthwave', '4k', 'aestheticism', 'arabesque', 'baroque', 'crystal cubism', 'futurism', 'hyperrealism', 'hypermodernism', 'retrofuturism', 'highly detailed', 'sharp focus', 'intricate', 'digital painting', 'illustration', 'octane render', 'smooth', '8 k', 'elegant', '8k', 'cinematic', '4k', 'fantasy', 'cinematic lighting', 'detailed', '4 k', 'photorealistic', 'unreal engine', 'masterpiece', 'realistic', 'hd', 'dramatic lighting', 'volumetric lighting', 'high detail', 'beautiful', 'hyperrealistic', 'hyper detailed', 'portrait', 'high quality', 'hyper realistic', 'ultra realistic', 'unreal engine 5', 'matte painting', 'ultra detailed', 'award winning', 'matte', 'intricate details', 'epic', 'hdr', 'extremely detailed', 'very detailed', 'oil painting', 'dynamic lighting', 'hyperdetailed', 'oil on canvas', 'high resolution', 'sharp', 'studio lighting', 'depth of field', 'hyper realism', 'full body', 'colorful', 'dark', 'cyberpunk', 'octane', 'centered', 'high contrast', 'bokeh', 'global illumination', 'dramatic', 'symmetrical', 'd & d', 'intricate detail', 'atmospheric', 'd&d', 'golden ratio', 'wide angle', 'anime', 'golden hour', 'detailed face', 'rule of thirds', 'stunning', 'vibrant colors', 'soft lighting', 'ray tracing', 'photography', 'epic composition', 'radiant light', '8k resolution', 'cinematic composition', 'horror', 'vibrant', 'black and white', 'close up', 'medium shot', 'ornate', 'photo realistic', 'photo', 'vivid colors', '8 k resolution', 'trending on art station', 'futuristic', 'high details', 'high definition', 'volumetric light', 'wide shot', 'movie still', 'establishing shot', 'surreal', 'gothic', '3 d render', 'unreal 5', 'photograph', 'sunset', 'painting', 'sci-fi', 'rim light', 'hearthstone', 'beautiful lighting', 'ambient lighting', 'sci - fi', 'moody', 'digital illustration', 'symmetrical face', 'epic lighting', 'fog', 'sakimichan', 'beautiful face', 'soft light', 'fine details', 'dark fantasy', 'gorgeous', 'deep focus', 'night', '3 d', 'eerie', 'daz', 'shallow depth of field', '3d', 'atmospheric lighting', 'symmetry', 'cute', 'studio quality', 'scifi', 'chiaroscuro', 'insanely detailed', 'white background', 'natural light', 'details', 'hard edges', 'character design', 'artistic', 'asymmetrical', 'rpg portrait', 'sunny day', 'ethereal', 'bright colors', 'raytracing', 'perfect face', 'god rays', 'psychedelic', '35mm', 'dramatic light', 'dynamic pose', '3 5 mm', 'steampunk', 'focus', 'extreme detail', 'lifelike', 'organic painting', 'backlit', '3d render', 'octane rendered', 'neon', 'foggy', 'uhd', '4k resolution', 'cinematic shot', 'breathtaking', 'vivid', 'fantasy', 'ultra wide angle', 'subsurface scattering', 'very coherent', 'stylized', 'ultra hd', 'aesthetic', 'lens flare', 'intricate linework', 'vintage', 'focused', 'insanely detailed and intricate', 'hq', 'ultrarealistic', 'dreamy', 'high fantasy', 'award winning photo', 'minimalist', '4 k resolution', 'award-winning', 'manga', 'realistic shaded perfect', 'professional photography', 'perfect composition', 'dslr', 'render', 'award winning photography', 'magical', 'abstract', 'detailed illustration', 'vaporwave', 'atmosphere', 'beautiful composition', 'bouguereau', 'glowing lights', 'cel shaded', 'painted', 'sense of awe', 'full of colour', 'detailed painting', 'synthwave', 'dark atmosphere', 'neo-gothic', 'realistic lighting', 'mystical', 'science fiction', 'cinematic atmosphere'];
+const flavors = ['vaporwave', 'cyberpunk', 'synthwave', '4k', 'aestheticism', 'arabesque', 'baroque', 'crystal cubism', 'futurism', 'hyperrealism', 'hypermodernism', 'retrofuturism', 'highly detailed', 'sharp focus', 'intricate', 'digital painting', 'illustration', 'octane render', 'smooth', '8 k', 'elegant', '8k', 'cinematic', '4k', 'fantasy', 'cinematic lighting', 'detailed', '4 k', 'photorealistic', 'unreal engine', 'masterpiece', 'realistic', 'hd', 'dramatic lighting', 'volumetric lighting', 'high detail', 'beautiful', 'hyperrealistic', 'hyper detailed', 'portrait', 'high quality', 'hyper realistic', 'ultra realistic', 'unreal engine 5', 'matte painting', 'ultra detailed', 'award winning', 'matte', 'intricate details', 'epic', 'hdr', 'extremely detailed', 'very detailed', 'oil painting', 'dynamic lighting', 'hyperdetailed', 'oil on canvas', 'high resolution', 'sharp', 'studio lighting', 'depth of field', 'hyper realism', 'full body', 'colorful', 'dark', 'cyberpunk', 'octane', 'centered', 'high contrast', 'bokeh', 'global illumination', 'dramatic', 'symmetrical', 'd & d', 'intricate detail', 'atmospheric', 'd&d', 'golden ratio', 'wide angle', 'anime', 'golden hour', 'detailed face', 'rule of thirds', 'stunning', 'vibrant colors', 'soft lighting', 'ray tracing', 'photography', 'epic composition', 'radiant light', '8k resolution', 'cinematic composition', 'horror', 'vibrant', 'black and white', 'close up', 'medium shot', 'ornate', 'photo realistic', 'photo', 'vivid colors', '8 k resolution', 'trending on art station', 'futuristic', 'high details', 'high definition', 'volumetric light', 'wide shot', 'movie still', 'establishing shot', 'surreal', 'gothic', '3 d render', 'unreal 5', 'photograph', 'sunset', 'painting', 'sci-fi', 'rim light', 'hearthstone', 'beautiful lighting', 'ambient lighting', 'sci - fi', 'moody', 'digital illustration', 'symmetrical face', 'epic lighting', 'fog', 'sakimichan', 'beautiful face', 'soft light', 'fine details', 'dark fantasy', 'gorgeous', 'deep focus', 'night', '3 d', 'eerie', 'daz', 'shallow depth of field', '3d', 'atmospheric lighting', 'symmetry', 'cute', 'studio quality', 'scifi', 'chiaroscuro', 'insanely detailed', 'white background', 'natural light', 'details', 'hard edges', 'character design', 'artistic', 'asymmetrical', 'rpg portrait', 'sunny day', 'ethereal', 'bright colors', 'raytracing', 'perfect face', 'god rays', 'psychedelic', '35mm', 'dramatic light', 'dynamic pose', '3 5 mm', 'steampunk'];
+
+const promptprompt = `
+I am generating text prompts for AI generators. These are descriptions of artworks which are highly detailed and aesthetically imaginative, and diverse. They should start with a description of the content and then a long list of modifiers which elaborate on the style, genre, and medium. They should be creative, bold, evocative, edgy, and eclectic.
+
+Here are some examples.
+
+Prototype of an isometric diorama, particle effects, 2.2 gama, sony a7r7, tamron 10-24mm f/3.5-4.5, iso 3200, extremely detailed, 8k texture, lots of flowers and vibrant plants
+A steampunk fox fursona with boots sitting on a vespa moped with sunglasses, vector drawing, graphic novel, grunge, geometric
+A cat chef making ramen, cartoon style, comic style, manga style
+Sheikh Zayed Grand Mosque in Abu Dhabi, extremely detailed digital painting, matte colors, rim light, beautiful Lighting, 8k, stunning scene, raytracing, octane
+Pikachu doing deadlifts and getting swole at a luxury gym, portra 400, gym shot, render in unreal engine
+Tall green hedge maze with an evil monster in shadows, light pink smoke, geometric, minimal, vintage, creepy
+
+I want you to generate a list of 10-20 such text prompts.
+`;
 
 
-export async function generatePrompt() {
+function removeNumbering(line) {
+  return line.replace(/^\d+[:.)]/, '').trim();
+}
+
+export async function combinePrompt() {
   const subject = getRandomSample(subjects, 1);
   const context = getRandomSample(contexts, 1);
   const flavors2 = getRandomSample(flavors, 2);
@@ -64,46 +83,28 @@ export async function generatePrompt() {
   return prompt;
 }
 
-export async function generatePrompts(n) {
-  let prompts = [];
-  for (var i=0; i<n; i++) {
-    const prompt = generatePrompt();
-    prompts.push(prompt);
-  }
-  return prompts;
-}
-
-export async function createPrompt() {
+export async function generatePrompts() {
   
-  const prompts = await generatePrompts(6);
-  const combined_prompt = prompts.join('\n')+'\n';
-
-  // combine into one string with newlines
-  const initString = "Make a detailed imaginitive description of a detailed artwork in the following style. Here are some examples. Create another 1."
-  const promptString = initString + prompts.join('\n');
-
   const configuration = new openai.Configuration({
     apiKey: OPENAI_API_KEY,
   });
   const openai_api = new openai.OpenAIApi(configuration);
 
-  let title = '';
-
-  while (title.length == 0) {
-    let completion = await openai_api.createCompletion({
-      model: "davinci",
-      prompt: combined_prompt,
-      temperature: 0.99,
-      max_tokens: 60,
-      top_p: 1,
-      frequency_penalty: 2.0,
-      presence_penalty: 1.0,
-      stop: ["\n"]
-    });
-    title = completion.data.choices[0].text;
-  }
-
-  return title.trim();
+  let completion = await openai_api.createCompletion({
+    model: "text-davinci-003",
+    prompt: promptprompt,
+    temperature: 0.9,
+    max_tokens: 200,
+    top_p: 1,
+    frequency_penalty: 0.15,
+    presence_penalty: 0.1
+  });
+    
+  let prompts = completion.data.choices[0].text;
+  prompts = prompts.split("\n");
+  prompts = prompts.filter((line) => line.length > 0);
+  prompts = prompts.map(removeNumbering);
+  
+  return prompts;
 }
   
-
--- index.js
+++ index.js
@@ -1,18 +1,19 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
-import axios from 'axios';
 import { EdenClient } from "eden-sdk";
-import {generatePrompt} from './config_generators.js';
+import {generatePrompts} from './config_generators.js';
 
 dotenv.config()
 
-const API_KEY = process.env.API_KEY;
-const API_SECRET = process.env.API_SECRET;
+const API_KEY = process.env.EDEN_API_KEY;
+const API_SECRET = process.env.EDEN_API_SECRET;
 const PORT = process.env.PORT || 8000;
 
+let eden = new EdenClient(API_KEY, API_SECRET);
+
 let nMade = 0;
-let nRunning = 3;
+let nRunning = 2;
 let nCompletions = 0;
 
 async function sleep(ms) {
@@ -21,30 +22,52 @@ async function sleep(ms) {
 
 const main = async () => {
 
-  const prompt = await generatePrompt();
+  const prompts = await generatePrompts();
   
-  console.log(prompt);
+  for (var i=0; i<prompts.length; i++) {
+    let prompt = prompts[i];
+    console.log("==========")
+    console.log(prompt)
 
-  let eden = new EdenClient(API_KEY, API_SECRET);
+    let sizes = [[576, 576], [640, 640], [832, 576], [800, 576], [768, 512], [800, 512]];
+    let steps = [100, 120, 80, 90, 100];
+    let samplers = ["euler", "euler_ancestral", "euler", "klms"];
 
-  eden.loginApi(
-    "admin",
-    "admin"
-  )
+    let size = sizes[Math.floor(Math.random() * sizes.length)];
+    if (Math.random() < 0.25) {
+      size = [size[1], size[0]];
+    }
 
-  let config = {
-    "text_input": prompt
-  }
+    let sampler = samplers[Math.floor(Math.random() * samplers.length)];
+    let numSteps = steps[Math.floor(Math.random() * steps.length)];
+    let W = size[0];
+    let H = size[1];
+    let scale = 5.0 + 8.0*Math.random();
+    let upscale_f = 1.0 + 1.0*Math.random();
 
-  try {
-    let result = await eden.create("create", config);
-    console.log(result);
-    nMade++;
-  } catch (error) {
-    console.error(error);
-    await sleep(1000);
-  } finally {
-    await sleep(1000);
+    let config = {
+      "mode": "generate", 
+      "text_input": prompt,
+      "sampler": sampler,
+      "scale": scale,
+      "steps": numSteps, 
+      "width": W,
+      "height": H,
+      "seed": Math.floor(1e8 * Math.random()),
+      "upscale_f": upscale_f,
+    }
+
+    try {
+      let result = await eden.create("create", config);
+      console.log(result);
+      nMade++;
+      await sleep(1000 * 60 * 4);
+    } catch (error) {
+      console.error(error);
+      await sleep(1000);
+    } finally {
+      await sleep(1000);
+    }
   }
 }
 
@@ -63,6 +86,8 @@ const app = express();
 app.use(cors());
 app.use(express.json({limit: '50mb'}));
 app.use(express.urlencoded({limit: '50mb'}));
+
+app.post("/update", handleUpdate);
 
 app.get("/", async (req, res) => {
   res.send(`Runner has made ${nMade} creations so far. ${nRunning} threads are running. Completions ${nCompletions}.`);
