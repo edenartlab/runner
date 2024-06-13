efc64273958be919c9a221fe25b3b18bea435343
Gene Kogan <kogan.gene@gmail.com>
2024-06-13 12:33:48 -0700

    Update main.py

--- main.py
+++ main.py
@@ -5,15 +5,15 @@ from runner import *
 
 
 def main():
-    thread1 = threading.Thread(target=run_comfy_heartbeat)
+    #thread1 = threading.Thread(target=run_comfy_heartbeat)
     #thread2 = threading.Thread(target=run_video_heartbeat)
     thread3 = threading.Thread(target=run_abraham_loop)
 
-    thread1.start()
+    #thread1.start()
     #thread2.start()
     thread3.start()
 
-    thread1.join()
+    #thread1.join()
     #thread2.join()
     thread3.join()
     
